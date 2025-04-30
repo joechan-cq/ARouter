0f0a2fa7cc2daaec404bb95139ca9efac40170fc
Joe Chan <joechan-cq@outlook.com>
2025-04-30 09:43:09 +0800

    remove unused dependency

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,9 +12,6 @@ dependencies {
     implementation(gradleApi())
     implementation(localGroovy())
     implementation("com.android.tools.build:gradle:8.1.4")
-    implementation("com.alibaba:fastjson:1.2.69")
-    // 更新 javapoet 版本
-    implementation("com.squareup:javapoet:1.13.0")
     // 更新 commons-codec 版本
     implementation("commons-codec:commons-codec:1.11")
     implementation("commons-io:commons-io:2.4")
